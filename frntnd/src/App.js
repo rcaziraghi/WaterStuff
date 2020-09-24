9b49e47e8609d0c447ee9c4073e8763dabb20ab4
Rafael Caziraghi <rcaziraghi@gmail.com>
2020-09-23 21:59:04 -0300

    Ajustado index.

--- frntnd/src/App.js
+++ frntnd/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from './waterstuff.jpg';
 import './App.css';
 
-function App() {
-  return (
+// function App() {
+
+// }
+
+const App = () => {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
