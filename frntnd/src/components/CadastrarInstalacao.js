9b1f0a70a03e9c4c71cca0e6fca607eb7fc299a3
Rafael Caziraghi <rcaziraghi@gmail.com>
2020-10-10 23:47:37 -0300

    Função de validação de CPF adicionada.

--- frntnd/src/components/CadastrarInstalacao.js
+++ frntnd/src/components/CadastrarInstalacao.js
@@ -17,6 +17,60 @@ const validarRequerido = (value) => {
   }
 };
 
+const validarCPF = (cpf) => {	
+	cpf = cpf.replace(/[^\d]+/g,'');	
+	if(cpf === '') {
+    return (
+      <div className="alert alert-danger" role="alert">
+        Este campo é requerido!
+      </div>
+    );
+  }
+	// Elimina CPFs invalidos conhecidos	
+	if (cpf.length !== 11 || 
+		cpf === "00000000000" || 
+		cpf === "11111111111" || 
+		cpf === "22222222222" || 
+		cpf === "33333333333" || 
+		cpf === "44444444444" || 
+		cpf === "55555555555" || 
+		cpf === "66666666666" || 
+		cpf === "77777777777" || 
+		cpf === "88888888888" || 
+		cpf === "99999999999")
+      return (
+        <div className="alert alert-danger" role="alert">
+          CPF inválido!
+        </div>
+      );	
+	// Valida 1o digito	
+	let add = 0;	
+	for (let i=0; i < 9; i ++)		
+		add += parseInt(cpf.charAt(i)) * (10 - i);	
+		let rev = 11 - (add % 11);	
+		if (rev === 10 || rev === 11)		
+			rev = 0;	
+		if (rev !== parseInt(cpf.charAt(9)))		
+      return (
+        <div className="alert alert-danger" role="alert">
+          CPF inválido!
+        </div>
+      );		
+	// Valida 2o digito	
+	add = 0;	
+	for (let i = 0; i < 10; i ++)		
+		add += parseInt(cpf.charAt(i)) * (11 - i);	
+	rev = 11 - (add % 11);	
+	if (rev === 10 || rev === 11)	
+		rev = 0;	
+	if (rev !== parseInt(cpf.charAt(10)))
+    return (
+      <div className="alert alert-danger" role="alert">
+        CPF inválido!
+      </div>
+    );		  
+}
+
 // pagina login
 const CadastrarInstalacao = (props) => {
   const form = useRef();
@@ -87,7 +141,7 @@ const CadastrarInstalacao = (props) => {
               name="cpf"
               value={cpf}
               onChange={aoMudarCpf}
-              validations={[validarRequerido]}
+              validations={[validarCPF]}
             />
           </div>
 
